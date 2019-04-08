523030c0281567d89a7c5a0b3c0b111608d8976f
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-04-08 17:44:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,8 +14,9 @@
 
 end
 
-def take_a_number(katz_deli, string)
-  
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(another_deli)
