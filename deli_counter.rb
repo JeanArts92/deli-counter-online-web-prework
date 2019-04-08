1ac710a6f5c0bb9aec626e1e27ecd74bfa9f24cd
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-04-08 16:38:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,10 +3,7 @@
 def line(katz_deli)
 other_line = other_deli
 another_line = another_deli
-all_lines = other_line << another_line
-new_array = []
-all_lines.each do |names|
-  new_array 
+
   puts "The line is currently empty. #{new_array}"
 end
 end
