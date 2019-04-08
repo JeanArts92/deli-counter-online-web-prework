56a0ec1bc9e203a6f42db1f6b430ccc6f0c97489
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-04-08 16:33:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+
+def line(katz_deli)
+other_line = other_deli
+another_line = another_deli
+all_lines = other_line << another_line
+new_array = []
+all_lines.each do
+  new_array
+end
+puts "The line is currently empty.#{new_array}"
+end
+
+def take_a_number(other_deli)
+  
+end
+
+def now_serving(another_deli)
+  
+end
+
