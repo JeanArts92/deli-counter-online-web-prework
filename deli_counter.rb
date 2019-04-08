3199f38cc1f0e4a46736c4f59c0eb98fe27bf8b8
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-04-08 17:31:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,17 @@
 # Write your code here.
 
-def line(katz_deli)
-  puts "The line is currently empty."
+  def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    message = "The line is currently:"
+
+    katz_deli.each_with_index do |value, index|
+      message += " #{index.to_i+1}. #{value}"
+    end
+
+    puts "#{message}"
+  end
   
 
 end
