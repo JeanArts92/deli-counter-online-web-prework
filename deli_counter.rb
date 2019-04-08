2d9fbc8c33d8fee3be342da5e1e4655cd9dbecc2
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-04-08 16:36:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ another_line = another_deli
 all_lines = other_line << another_line
 new_array = []
 all_lines.each do |names|
-  new_array
+  new_array << |names|
 end
 puts "The line is currently empty.#{new_array}"
 end
