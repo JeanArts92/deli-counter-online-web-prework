e62ec2e79adf7d62fe3dc0fc695497a9c34bb611
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-04-08 16:42:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,7 @@
 def line(katz_deli)
 other_line = other_deli
 another_line = another_deli
-all_lines =
-  puts "The line is currently empty. #{new_array}"
-end
+all_lines = other_line + another_line
 end
 
 def take_a_number(other_deli)
