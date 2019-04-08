fe29797e2b9f3d68785820f429c526c59a6843db
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-04-08 16:38:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,8 +7,8 @@ all_lines = other_line << another_line
 new_array = []
 all_lines.each do |names|
   new_array 
+  puts "The line is currently empty. #{new_array}"
 end
-puts "The line is currently empty.#{new_array}"
 end
 
 def take_a_number(other_deli)
